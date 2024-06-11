14f1254a2eff9f0389a81e9012314e39bee138ee
kine14 <74851601+zeuxi141@users.noreply.github.com>
2024-06-11 23:14:41 +0700

    Add env enviroment for development and production

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,10 +3,13 @@ import 'dotenv/config'
 
 export const env = {
   MONGODB_URI: process.env.MONGODB_URI,
+  DATABASE_NAME: process.env.DATABASE_NAME,
+
   APP_HOST: process.env.APP_HOST,
   APP_PORT: process.env.APP_PORT,
-  AUTHOR: process.env.AUTHOR,
-  DATABASE_NAME: process.env.DATABASE_NAME
 
+  BUILD_MODE: process.env.BUILD_MODE,
+
+  AUTHOR: process.env.AUTHOR
 
 }
