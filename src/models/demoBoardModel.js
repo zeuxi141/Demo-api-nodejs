9bb6a0c5cadc8cc61194654311f9dec23981ae36
kine14 <74851601+zeuxi141@users.noreply.github.com>
2024-06-21 21:14:50 +0700

    completed build aggreagte mongodb

--- src/models/demoBoardModel.js
+++ src/models/demoBoardModel.js
@@ -3,7 +3,10 @@
 import Joi from 'joi'
 import { ObjectId } from 'mongodb'
 import { GET_DB } from '~/config/mongodb'
+import { BOARD_TYPES } from '~/utils/constants'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
+import { cardModel } from './cardModel'
+import { columnModel } from './columnModel'
 
 
 //Định nghĩa collection (name và Schema)
@@ -12,6 +15,8 @@ const BOARD_SCHEMA = Joi.object({
   title: Joi.string().required().min(3).max(50).trim().strict(),
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
+
+  type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required(),
 
   columnOrderIds: Joi.array().items(
     Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
@@ -35,16 +40,34 @@ const createNew = async(data) => {
 }
 
 //chỉ lấy board theo id
-const findOneById = async(id) => {
+const getDetails = async(id) => {
   try {
-    return await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-      _id: new ObjectId(id)
-    })
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).aggregate([
+      { $match: {
+        _id: new ObjectId(id), //chỉ lấy board theo id
+        _destroy: false //chỉ lấy board không bị xóa
+      } },
+      { $lookup: {
+        from: columnModel.COLUMN_COLLECTION_NAME, //lookup từ collection column
+        //Tìm dữ liệu từ collection column nếu loaclField và foreignField trùng nhau
+        localField: '_id', //id cuả board hien tai
+        foreignField: 'boardId', //khoa ngoai lien ket toi collection ben column
+        as: 'columns' //Lấy collection bên column và lưu lại
+      } },
+      { $lookup: {
+        from: cardModel.CARD_COLLECTION_NAME,
+        localField: '_id',
+        foreignField: 'boardId',
+        as: 'cards'
+      } }
+    ]).toArray()//trả về dữ liệu theo dạng mảng
+    //Do dữ liệu chúng ta lấy chỉ có 1 board mà nó nằm trong mảng nên chúng ta chỉ cần lấy phần tử đầu tiên
+    return result[0] || {}
   } catch (error) { throw new Error(error)}
 }
 
 //Query tổng hợp (aggregate) để lấy thông tin toàn bộ cột và card trong board
-const getDetails = async(id) => {
+const findOneById = async(id) => {
   try {
     return await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
       _id: new ObjectId(id)
